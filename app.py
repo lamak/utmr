a5d6e01e615b8d89ce80fff360e8e56c73dc723e
Maxim Kapranov <max.lamak@gmail.com>
2019-11-15 16:49:36 +1000

    workaround in case utm selfdiagnosed problem with rsa and moved down by 1 div blocks

--- app.py
+++ app.py
@@ -186,7 +186,7 @@ def parse_utm(utm: Utm):
     try:
         homepage.go(utm.build_url())
         gostpage.go(utm.gost_url())
-
+        # todo: блоки нужно проверять явно на совпадение заголовка, нумерация может измениться
         # версия
         try:
             result.version = homepage.doc.select('//*[@id="home"]/div[1]/div[2]').text()
@@ -203,30 +203,32 @@ def parse_utm(utm: Utm):
         except DataNotFound:
             result.error.append('Не найден ФСРАР ид\n')
 
-        # ключи
-        try:
-            gost_string = homepage.doc.select('//*[@id="home"]/div[9]/div[2]').text()
-            pki_string = homepage.doc.select('//*[@id="home"]/div[8]/div[2]').text()
-            result.pki = last_date(pki_string)
-            result.gost = last_date(gost_string)
-        except (IndexError, DataNotFound):
-            result.error.append('Не найдены сроки ключей\n')
-
         # Самодиагностика
         try:
             status_string = homepage.doc.select('//*[@id="home"]/div[4]/div[2]').text()
-            license_string = homepage.doc.select('//*[@id="home"]/div[5]/div[2]').text()
+            result.status = 'RSA сертификат pki.fsrar.ru соответствует контуру' == status_string
+            # если сатус не соотвествует указанному,значит появился блок Проблема с RSA и нумерация блоков увеличилась
+            div_inc = not result.status
+            license_string = homepage.doc.select(f'//*[@id="home"]/div[{5 + div_inc}]/div[2]').text()
             filter_string = homepage.doc.select('//*[@id="filterMsgDiv"]').text()
             result.filter = 'Обновление настроек не требуется' == filter_string
-            result.status = 'RSA сертификат pki.fsrar.ru соответствует контуру' == status_string
             result.license = 'Лицензия на вид деятельности действует' == license_string
 
         except DataNotFound:
             result.error.append('Не найдены все элементы на странице\n')
 
+        # ключи
+        try:
+            gost_string = homepage.doc.select(f'//*[@id="home"]/div[{9 + div_inc}]/div[2]').text()
+            pki_string = homepage.doc.select(f'//*[@id="home"]/div[{8 + div_inc}]/div[2]').text()
+            result.pki = last_date(pki_string)
+            result.gost = last_date(gost_string)
+        except (IndexError, DataNotFound):
+            result.error.append('Не найдены сроки ключей\n')
+
         # Дата отправки последнего чека не должна быть старше одного дня
         try:
-            cheque_string = homepage.doc.select('// *[@id="home"]/div[7]/div[2]').text()
+            cheque_string = homepage.doc.select(f'// *[@id="home"]/div[{7 + div_inc}]/div[2]').text()
             today = datetime.strftime(datetime.now(), "%Y-%m-%d")
 
             if cheque_string == 'Отсутствуют неотправленные чеки':

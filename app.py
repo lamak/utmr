b5118b055a7309feccb1205d3e037ef72dca65f0
Maxim Kapranov <max.lamak@gmail.com>
2020-02-25 17:37:50 +1000

    create update utm to config file

--- app.py
+++ app.py
@@ -236,6 +236,16 @@ def import_utms_to_db():
     [create_update_utm_db(mongodb, u) for u in utms]
 
 
+def create_update_config_utm(config, utm: Utm):
+    with open(config, 'r') as f:
+        lines = f.read().splitlines()
+
+    with open(config, 'w') as f:
+        for l in lines:
+            if l.split(';')[0] != utm.fsrar:
+                f.write(l + '\n')
+
+
 class FsrarForm(FlaskForm):
     fsrar = SelectField('fsrar', choices=[(u.fsrar, f'{u.title} [{u.fsrar}] [{u.host}]') for u in get_utm_list()])
 
